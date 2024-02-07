b03dee940b879059b499488400e0982742015c8c
AragonD19 <88000475+AragonD19@users.noreply.github.com>
2024-02-06 19:08:00 -0600

    Update lexicalAnalyzer.py

--- lexicalAnalyzer.py
+++ lexicalAnalyzer.py
@@ -7,15 +7,13 @@
 #Importación de módulos y librerías para mostrar gráficamente los autómatas finitos.
 import networkx as nx
 import matplotlib.pyplot as plt
-from networkx.drawing.nx_agraph import graphviz_layout
 from collections import deque
-from collections import defaultdict
 
 #Algoritmo Shunting Yard para pasar una expresión infix a postfix.
 
 def insert_concatenation(expression): #Función insert_concatenation para poder agregar los operadores al arreglo result.
     result = [] #Lista result para agregar los operadores.
-    operators = "#+|*()?" #Operadores en la lista.
+    operators = "+|*()?" #Operadores en la lista.
     for i in range(len(expression)): #Por cada elemento según el rango de la variable expression:
         char = expression[i]
         result.append(char) #Se agrega caracter por caracter al arreglo.
@@ -36,13 +34,11 @@ def insert_concatenation(expression): #Función insert_concatenation para poder 
                 result.append('.')
             elif char == '?' and lookahead.isalnum():
                 result.append('.')
-            elif char == '#' and lookahead.isalnum():
-                result.append('.')
 
     return ''.join(result) #Devuelve el resultado.
 
 def shunting_yard(expression): #Función para realizar el algoritmo shunting yard.
-     precedence = {'#': 1,'+': 1, '|': 1, '.': 2, '*': 3, '?':3} # Orden de precedencia entre operadores.
+     precedence = {'+': 1, '|': 1, '.': 2, '*': 3, '?':3} # Orden de precedencia entre operadores.
 
      output_queue = [] #Lista de salida como notación postfix.
      operator_stack = []
@@ -54,7 +50,7 @@ def shunting_yard(expression): #Función para realizar el algoritmo shunting yar
          token = expression[i] #El token es igual al elemento en la lista en la posición i.
          if token.isalnum() or token == 'ε': #Si el token es una letra o un dígito, o es epsilon.
              output_queue.append(token) #Se agrega a output_queue.
-         elif token in "+|*?.#": #Si hay alguno de estos operadores en el token:
+         elif token in "+|*?.": #Si hay alguno de estos operadores en el token:
              while (operator_stack and operator_stack[-1] != '(' and #Se toma en cuenta la precedencia y el orden de operadores para luego añadirla al queue y a la pila.
                     precedence[token] <= precedence.get(operator_stack[-1], 0)):
                  output_queue.append(operator_stack.pop())
@@ -377,209 +373,7 @@ def afn_to_afd(afn):
 
 #Algoritmo de Construcción Directa para convertir una regex en un AFD.
 
-class Node:
-    def __init__(self, value):
-        self.value = value
-        self.left = None
-        self.right = None
 
-def build_syntax_tree(regex):
-    regex_postfix = shunting_yard(regex + '#')  # Convertir la expresión regular a formato postfix con '#' al final
-    stack = []
-    nodes_calculated = set()  # Conjunto para rastrear qué nodos ya han sido calculados
-    position_counter = 1  # Contador para asignar números de posición
-
-    for char in regex_postfix:
-        if char.isalnum() or char == 'ε':
-            node = Node(char)
-            node.position = position_counter
-            position_counter += 1
-            stack.append(node)
-            nodes_calculated.add(node)
-        elif char in ".|*+?#":  # Operadores
-            if char == '.':
-                if len(stack) < 2:
-                    raise ValueError("Insuficientes operandos para la concatenación")
-                right = stack.pop()
-                left = stack.pop()
-                print(f"Concatenando nodos {left.value} y {right.value}")
-                node = Node('.')
-                node.left = left
-                node.position = position_counter
-                position_counter += 1
-                if isinstance(right, Node) and right.value == '#':
-                    node.right = right
-                else:
-                    node.right = right
-                stack.append(node)
-            elif char == '|':
-                right = stack.pop()
-                left = stack.pop()
-                print(f"Creando nodo OR con hijos {left.value} y {right.value}")
-                node = Node('|')
-                node.left = left
-                node.right = right
-                node.position = position_counter
-                position_counter += 1
-                stack.append(node)
-            elif char == '*':
-                child = stack.pop()
-                print(f"Creando nodo Kleene con hijo {child.value}")
-                node = Node('*')
-                node.left = child
-                node.position = position_counter
-                position_counter += 1
-                stack.append(node)
-            elif char == '+':
-                child = stack.pop()
-                print(f"Creando nodo Positivo con hijo {child.value}")
-                node = Node('+')
-                node.left = child
-                node.position = position_counter
-                position_counter += 1
-                stack.append(node)
-            elif char == '?':
-                child = stack.pop()
-                print(f"Creando nodo Opcional con hijo {child.value}")
-                node = Node('?')
-                node.left = child
-                node.position = position_counter
-                position_counter += 1
-                stack.append(node)
-            elif char == '#':
-                if stack:
-                    child = stack.pop()
-                    if isinstance(child, Node) and child.value == '.':
-                        node = Node('.')
-                        node.left = child.left
-                        node.right = Node('#')
-                        node.position = position_counter
-                        position_counter += 1
-                        print(f"Creando nodo concatenación con hijo izquierdo {child.left.value} y hijo derecho #")
-                        stack.append(node)
-                    else:
-                        node = Node('#')
-                        node.right = child
-                        node.position = position_counter
-                        position_counter += 1
-                        print(f"Creando nodo marcador final con hijo {child.value}")
-                        stack.append(node)
-                else:
-                    node = Node('#')
-                    node.position = position_counter
-                    position_counter += 1
-                    print("Creando nodo marcador final sin hijos")
-                    stack.append(node)
-
-    return stack.pop(), nodes_calculated
-
-def visualize_tree(root):
-    G = nx.DiGraph()
-    build_networkx_graph(root, G)
-
-    # Ajusta el parámetro scale para aumentar la distancia entre los nodos hijos
-    pos = nx.kamada_kawai_layout(G, scale=100.0)
-
-    labels = {node: node.value for node in G.nodes()}
-    nx.draw(G, pos, labels=labels, with_labels=True, node_size=700, node_color="skyblue", font_size=15, font_weight="bold")
-    plt.show()
-
-def build_networkx_graph(root, G):
-    if root is not None:
-        stack = [root]  # Usamos una pila para realizar DFS
-
-        while stack:
-            current_node = stack.pop()
-            G.add_node(current_node)
-
-            if current_node.left:
-                stack.append(current_node.left)
-                G.add_node(current_node.left)
-                G.add_edge(current_node, current_node.left)
-
-            if current_node.right:
-                stack.append(current_node.right)
-                G.add_node(current_node.right)
-                G.add_edge(current_node, current_node.right)
-
-            if not current_node.left and not current_node.right:
-                G.add_node(current_node)
-
-def get_all_nodes(node):
-    nodes = set()
-
-    if node is not None:
-        nodes.add(node)
-        nodes |= get_all_nodes(node.left)
-        nodes |= get_all_nodes(node.right)
-
-    return nodes
-
-def nullable(node):
-    if node.value == 'ε':
-        return True
-    elif node.value == '.':
-        return nullable(node.left) and nullable(node.right)
-    elif node.value == '|':
-        return nullable(node.left) or nullable(node.right)
-    elif node.value == '*':
-        return True
-    elif node.value == '+':
-        return nullable(node.left)
-    elif node.value == '?':
-        return True if nullable(node.left) else nullable(node.right)
-
-def firstpos(node):
-    if node.value.isalnum() or node.value == 'ε':
-        return {id(node)}
-    elif node.value == '.':
-        if nullable(node.left):
-            return firstpos(node.left) | firstpos(node.right)
-        else:
-            return firstpos(node.left)
-    elif node.value == '|':
-        return firstpos(node.left) | firstpos(node.right)
-    elif node.value == '*':
-        return firstpos(node.left)
-    elif node.value == '+':
-        return firstpos(node.left)
-    elif node.value == '?':
-        return firstpos(node.left)
-
-def lastpos(node):
-    if node.value.isalnum() or node.value == 'ε':
-        return {id(node)}
-    elif node.value == '.':
-        if nullable(node.right):
-            return lastpos(node.left) | lastpos(node.right)
-        else:
-            return lastpos(node.right)
-    elif node.value == '|':
-        return lastpos(node.left) | lastpos(node.right)
-    elif node.value == '*':
-        return lastpos(node.left)
-    elif node.value == '+':
-        return lastpos(node.left)
-    elif node.value == '?':
-        return lastpos(node.left)
-
-def followpos(node):
-    if node.value == '.':
-        for pos in lastpos(node.left):
-            for fp in firstpos(node.right):
-                follow_pos[pos].update(fp)
-    elif node.value == '*':
-        for pos in lastpos(node.left):
-            for fp in firstpos(node):
-                follow_pos[pos].update(fp)
-    elif node.value == '|':
-        pass  # No se necesita hacer nada para operador de unión
-    elif node.value == '+':
-        for pos in lastpos(node.left):
-            for fp in firstpos(node.left):
-                follow_pos[pos].update(fp)
-    elif node.value == '?':
-        pass  # No se necesita hacer nada para operador opcional    
 
 #Algoritmo de Hopcroft para minimizar un AFD por medio de construcción de subconjuntos.
 
@@ -662,10 +456,12 @@ def remove_unreachable_states(dfa):
 #Simulación:
 
 if __name__ == "__main__":
+    expression = input("Enter your infix expression: ") 
+    postfix_expression = shunting_yard(expression) 
+    print("Postfix expression:", postfix_expression) 
+
     regex = input("Enter a regular expression: ")
     w = input("Enter a string to check: ")
-    postfix_expression = shunting_yard(regex) 
-    print("Postfix expression:", postfix_expression) 
 
     afn, accept_state = regex_to_afn(regex, 0)
     print("afn edges", afn.edges(data='label'))
@@ -786,20 +582,3 @@ if __name__ == "__main__":
         print(f"'{w}' pertenece al lenguaje L({regex})")
     else:
         print(f"'{w}' no pertenece al lenguaje L({regex})")
-
-    #Construcción directa (AFD).
-        
-    syntax_tree, nodes_calculated = build_syntax_tree(regex)
-    print("Árbol Sintáctico:")
-    visualize_tree(syntax_tree)
-
-    # Construir followpos
-    follow_pos = {node.position: set() for node in nodes_calculated}  # Usar solo los nodos calculados
-    for node in nodes_calculated:
-        followpos(node)
-
-    # Imprimir followpos para cada posición
-    print("\nFollowpos:")
-    for pos, fp in follow_pos.items():
-        values = ", ".join(str(node.position) for node in nodes_calculated if node.position in fp)
-        print(f"Posición {pos}: {{{values}}}")
